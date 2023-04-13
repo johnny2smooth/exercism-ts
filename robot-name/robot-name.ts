dcd514dbe89060a64441fc513d32902311a5a548
johnny2smooth <johnbp13@gmail.com>
2023-04-13 14:55:05 -0700

    4/13 robots

--- robot-name/robot-name.ts
+++ robot-name/robot-name.ts
@@ -1,15 +1,52 @@
 export class Robot {
-  constructor() {}
+  nameStart: string;
+  nameEnd: number;
+  robotName: string;
+
+  private static allRobots = new Set<string>();
+
+  constructor() {
+    this.nameStart = this.createNameStart();
+    this.nameEnd = this.createNameEnd();
+    this.robotName = this.nameStart + this.nameEnd;
+    Robot.allRobots.add(this.robotName);
+  }
 
   public get name(): string {
-    throw new Error('Implement Robot#name')
+    return this.robotName;
   }
 
   public resetName(): void {
-    throw new Error('Implement Robot#resetName')
+    let placeholderName = this.createNameStart() + this.createNameEnd();
+    do {
+      placeholderName = this.createNameStart() + this.createNameEnd();
+    } while (Robot.allRobots.has(placeholderName));
+    this.robotName = placeholderName;
+    Robot.allRobots.add(this.robotName);
   }
 
   public static releaseNames(): void {
-    throw new Error('Implement Robot.releaseNames')
+    Robot.allRobots.clear();
+  }
+
+  private createRandomString() {
+    return (Math.random() + 1).toString(36).split("");
+  }
+
+  private createNameStart() {
+    return this.createRandomString()
+      .filter((char) => char.match(/[a-z]/))
+      .slice(0, 2)
+      .join("")
+      .toUpperCase();
+  }
+  private createNameEnd() {
+    let nameEnd = Math.trunc(Math.random() * 1000);
+    if (nameEnd.toString().split("").length === 2) {
+      nameEnd = Number(nameEnd.toString() + Math.random() * 10);
+    }
+    return nameEnd;
   }
 }
+
+console.log();
