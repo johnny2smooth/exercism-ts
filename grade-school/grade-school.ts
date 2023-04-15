6e188afe729056533bb4ef17266f00c47c1f19c2
johnny2smooth <johnbp13@gmail.com>
2023-04-15 09:12:41 -0700

    4/15

--- grade-school/grade-school.ts
+++ grade-school/grade-school.ts
@@ -1,13 +1,40 @@
+type Roster = { [keyof: string]: string[] };
 export class GradeSchool {
-  roster() {
-    throw new Error('Remove this statement and implement this function')
+  private allStudents: Roster;
+  constructor() {
+    this.allStudents = {};
   }
 
-  add() {
-    throw new Error('Remove this statement and implement this function')
+  roster(): Roster {
+    let sortedRoster: Roster = {};
+    let sortedGrades = Object.keys(this.allStudents).sort(
+      (a: string, b: string) => a.localeCompare(b)
+    );
+    sortedGrades.forEach((grade) => {
+      // Deep copy of the grade array to prevent modifications
+      sortedRoster[grade] = [...this.allStudents[grade]];
+    });
+    return sortedRoster;
+  }
+  add(name: string, grade: number) {
+    for (const existingGrade in this.allStudents) {
+      const index = this.allStudents[existingGrade].indexOf(name);
+      if (index > -1) {
+        this.allStudents[existingGrade].splice(index, 1);
+      }
+    }
+
+    let gradeArr: string[] = this.grade(grade);
+    let sortedArray = gradeArr
+      .concat([name])
+      .sort((a, b) => a.localeCompare(b));
+    this.allStudents[grade] = sortedArray;
   }
 
-  grade() {
-    throw new Error('Remove this statement and implement this function')
+  public grade(grade: number) {
+    if (!this.allStudents[grade]) {
+      this.allStudents[grade] = [];
+    }
+    return [...this.allStudents[grade]];
   }
 }
