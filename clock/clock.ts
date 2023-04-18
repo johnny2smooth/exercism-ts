3d80a1b33cfead1cd44bba8a9d7d8185467a02c5
johnny2smooth <johnbp13@gmail.com>
2023-04-18 06:51:21 -0700

    4/18

--- clock/clock.ts
+++ clock/clock.ts
@@ -16,16 +16,32 @@ export class Clock {
     }`;
   }
 
-  public plus(minutes: unknown): Clock {
-    throw new Error("Remove this statement and implement this function");
+  public plus(minutes: number) {
+    let {
+      minute: additionalMinute,
+      hour: additionalHour,
+    }: { minute: number; hour: number } = this.handleMinute(minutes);
+    let minAndHour = this.handleMinute(additionalMinute + this.minute);
+    this.minute = minAndHour.minute;
+    this.hour += this.handleHour(additionalHour + minAndHour.hour);
+    this.hour = this.handleHour(this.hour);
+    return this;
   }
 
-  public minus(minutes: unknown): Clock {
-    throw new Error("Remove this statement and implement this function");
+  public minus(minutes: number) {
+    let {
+      minute: additionalMinute,
+      hour: additionalHour,
+    }: { minute: number; hour: number } = this.handleMinute(minutes);
+    let minAndHour = this.handleMinute(-additionalMinute + this.minute);
+    this.minute = minAndHour.minute;
+    this.hour -= this.handleHour(additionalHour - minAndHour.hour);
+    this.hour = this.handleHour(this.hour);
+    return this;
   }
 
-  public equals(other: unknown): unknown {
-    throw new Error("Remove this statement and implement this function");
+  public equals(newClock: Clock) {
+    return this.handleFlattenHour() === newClock.toString();
   }
 
   private handleMinute(minute: number | undefined): {
@@ -33,21 +49,35 @@ export class Clock {
     hour: number;
   } {
     if (!minute) return { hour: 0, minute: 0 };
-    if (minute >= 60 || minute <= -60) {
+    let isNegative = minute < 0;
+    let isMoreThanHour = Math.abs(minute) >= 60;
+
+    if (isNegative) {
+      let remainder = minute % 60;
+      let hours = -1;
+      if (isMoreThanHour) {
+        hours += (minute - remainder) / 60;
+      }
+      return { minute: 60 - Math.abs(remainder), hour: hours };
+    }
+
+    if (isMoreThanHour) {
       let remainder = minute % 60;
       let hours = (minute - remainder) / 60;
       return { minute: remainder, hour: hours };
     }
     return { hour: 0, minute };
   }
+
   private handleHour(hour: number) {
     let hours = hour % 24;
     if (hours < 0) return 24 + hours;
     return hours;
   }
+
+  private handleFlattenHour() {
+    return `${this.hour >= 10 ? this.hour : `0${this.hour}`}:${
+      this.minute >= 10 ? this.minute : `0${this.minute}`
+    }`;
+  }
 }
-
-console.log(new Clock(-1, 15).toString());
-console.log(-1 % 24);
-
-// handle negative minutes
