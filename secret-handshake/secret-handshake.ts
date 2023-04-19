cba565279b69c9f59098b79d3d8dd7d02a672675
johnny2smooth <johnbp13@gmail.com>
2023-04-19 16:24:23 -0700

    kinda stuck

--- secret-handshake/secret-handshake.ts
+++ secret-handshake/secret-handshake.ts
@@ -1,3 +1,35 @@
-export function commands() {
-  throw new Error('Remove this statement and implement this function')
+const SECRETS = {
+  1: "wink",
+  10: "double blink",
+  100: "close your eyes",
+  1000: "jump",
+  10000: "reverse",
+};
+
+export function commands(decimal: number) {
+  if (!decimal) return [];
+  let handshakeArray: string[] = [];
+  const binary = convertDecimalToBinary(decimal);
+  let remainder = binary;
+
+  while (remainder > 0) {}
+
+  return handshakeArray;
 }
+
+console.log(commands(1));
+
+function convertDecimalToBinary(decimal: number) {
+  return +(decimal >>> 0).toString(2);
+}
+
+const test = convertDecimalToBinary(6);
+console.log(typeof test);
+console.log(test);
+console.log(test % 1);
+console.log(test % 10);
+console.log(test % 100);
+console.log(test % 1000);
+console.log(test % 10000);
+console.log(test % 100000);
+console.log(test.toString().length);
