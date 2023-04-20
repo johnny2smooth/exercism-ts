6744fe9f45b63dfb0cc8d8b94cf265dc290490a9
johnny2smooth <johnbp13@gmail.com>
2023-04-20 09:00:38 -0700

    first solution

--- secret-handshake/secret-handshake.ts
+++ secret-handshake/secret-handshake.ts
@@ -12,24 +12,51 @@ export function commands(decimal: number) {
   const binary = convertDecimalToBinary(decimal);
   let remainder = binary;
 
-  while (remainder > 0) {}
+  while (remainder > 0) {
+    if (remainder % 10 === 1) {
+      handshakeArray.push("wink");
+      remainder -= 1;
+    }
+    if ((remainder >= 10 && remainder < 100) || remainder % 100 === 10) {
+      handshakeArray.push("double blink");
+      remainder -= 10;
+    }
+    if ((remainder >= 100 && remainder < 1000) || remainder % 1000 === 100) {
+      handshakeArray.push("close your eyes");
+      remainder -= 100;
+    }
+    if (
+      (remainder >= 1000 && remainder < 10000) ||
+      remainder % 10000 === 1000
+    ) {
+      handshakeArray.push("jump");
+      remainder -= 1000;
+    }
+    if (remainder >= 10000) {
+      remainder -= 10000;
+    }
+  }
+
+  if (binary > 10000) {
+    handshakeArray.reverse();
+  }
 
   return handshakeArray;
 }
 
-console.log(commands(1));
+// console.log(commands(19));
 
 function convertDecimalToBinary(decimal: number) {
   return +(decimal >>> 0).toString(2);
 }
 
-const test = convertDecimalToBinary(6);
-console.log(typeof test);
-console.log(test);
-console.log(test % 1);
-console.log(test % 10);
-console.log(test % 100);
-console.log(test % 1000);
-console.log(test % 10000);
-console.log(test % 100000);
-console.log(test.toString().length);
+// const test = convertDecimalToBinary(16);
+// console.log(typeof test);
+// console.log(test);
+// console.log(test % 1);
+// console.log(test % 10);
+// console.log(test % 100);
+// console.log(test % 1000);
+// console.log(test % 10000);
+// console.log(test % 100000);
+// console.log(test);
