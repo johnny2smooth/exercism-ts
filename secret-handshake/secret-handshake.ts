f0ac280f644a84cbca793026ac96c1ad981139e1
johnny2smooth <johnbp13@gmail.com>
2023-04-20 09:04:27 -0700

    4/20

--- secret-handshake/secret-handshake.ts
+++ secret-handshake/secret-handshake.ts
@@ -1,3 +1,4 @@
+// process.env.SECRETS
 const SECRETS = {
   1: "wink",
   10: "double blink",
@@ -5,31 +6,28 @@ const SECRETS = {
   1000: "jump",
   10000: "reverse",
 };
-
+// So that we can give the equation to friends and they can
+// make their own secrets without knowing ours
 export function commands(decimal: number) {
   if (!decimal) return [];
   let handshakeArray: string[] = [];
   const binary = convertDecimalToBinary(decimal);
   let remainder = binary;
-
   while (remainder > 0) {
     if (remainder % 10 === 1) {
-      handshakeArray.push("wink");
+      handshakeArray.push(SECRETS[1]);
       remainder -= 1;
     }
-    if ((remainder >= 10 && remainder < 100) || remainder % 100 === 10) {
-      handshakeArray.push("double blink");
+    if (remainder % 100 === 10) {
+      handshakeArray.push(SECRETS[10]);
       remainder -= 10;
     }
-    if ((remainder >= 100 && remainder < 1000) || remainder % 1000 === 100) {
-      handshakeArray.push("close your eyes");
+    if (remainder % 1000 === 100) {
+      handshakeArray.push(SECRETS[100]);
       remainder -= 100;
     }
-    if (
-      (remainder >= 1000 && remainder < 10000) ||
-      remainder % 10000 === 1000
-    ) {
-      handshakeArray.push("jump");
+    if (remainder % 10000 === 1000) {
+      handshakeArray.push(SECRETS[1000]);
       remainder -= 1000;
     }
     if (remainder >= 10000) {
@@ -40,23 +38,8 @@ export function commands(decimal: number) {
   if (binary > 10000) {
     handshakeArray.reverse();
   }
-
   return handshakeArray;
 }
-
-// console.log(commands(19));
-
 function convertDecimalToBinary(decimal: number) {
   return +(decimal >>> 0).toString(2);
 }
-
-// const test = convertDecimalToBinary(16);
-// console.log(typeof test);
-// console.log(test);
-// console.log(test % 1);
-// console.log(test % 10);
-// console.log(test % 100);
-// console.log(test % 1000);
-// console.log(test % 10000);
-// console.log(test % 100000);
-// console.log(test);
